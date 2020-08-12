48d417cba183a963c9a8cba34da588a34e34e2f2
Sebastian Rodarte <sebrodval@gmail.com>
2020-08-11 20:32:23 -0500

    cambio a main en development

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,8 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("master");
+        
+        System.out.println("development1");
     }
     
 }
