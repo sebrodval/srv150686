5571d40f45db0655748c8cf0be56fab573bd2b6a
Sebastian Rodarte <sebrodval@gmail.com>
2020-08-11 20:10:17 -0500

    primer commit master

---
+++ TestGit/src/testgit/TestGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author sebro
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("master");
+    }
+    
+}
