c3485e3f1ce1eb768b3588b3d7d9414086370142
Sebastian Rodarte <sebrodval@gmail.com>
2020-08-11 20:36:16 -0500

    segundo cambio en main de development

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,7 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("development1");
+        System.out.println("development2");
     }
     
 }
