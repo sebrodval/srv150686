07a3bde4f2d33af898b093d7428e940d8e7127ca
Sebastian Rodarte <sebrodval@gmail.com>
2020-08-11 20:48:34 -0500

    subiendo qa

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,7 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("development2");
+        System.out.println("QA");
     }
     
 }
